971cb3c6ec37ac4b433aa3ae2d6cd64a9ed7fe04
gladensum13 <153849437+gladensum13@users.noreply.github.com>
2024-01-29 07:56:29 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,14 @@ import os
 
 def main():
     model_type = 'restormer'
-    model_file_name = '/ImageRestoration-CV/restormer_model.pth'
+    model_file_name = '/weights/restormer_model.pth'
     # Restormer is trained on  focal planes of 0,20,70 cm , if you provide just one image it is multiplicated 3x
-
+    
     input_img_folder  = '/input_images/'
-
     output_folder = '/Results'
-
+    
+     # Initialize model handler
+    model_handler = UserModelHandler(model_type=model_type, model_file_name=model_file_name)
 
   
     psnr_scores = []
@@ -27,8 +28,8 @@ def main():
 
         
 
-            # Initialize model handler
-            model_handler = UserModelHandler(model_type=model_type, model_file_name=model_file_name)
+           
+    
 
             # Load inputs
             input,gt,bool_gt = model_handler.load_inputs(f'{subdir}/{dir}')
